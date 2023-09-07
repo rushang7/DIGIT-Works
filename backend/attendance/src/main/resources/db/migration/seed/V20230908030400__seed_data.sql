48c468849dc43a8ab7a2da30282618132c63cc48
rushang7 <rushangdhanesha@gmail.com>
2023-09-08 03:43:35 +0530

    Setup H2 DB

---
+++ backend/attendance/src/main/resources/db/migration/seed/V20230908030400__seed_data.sql
@@ -0,0 +1,179 @@
+INSERT INTO eg_wms_attendance_register (
+    id,
+    tenantid,
+    registernumber,
+    name,
+    startdate,
+    enddate,
+    status,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '1', -- Replace with the desired value for id
+    'tenant123', -- Replace with the desired value for tenantid
+    '1', -- Replace with the desired value for registernumber
+    'John Doe', -- Replace with the desired value for name
+    1631097600, -- Replace with the desired value for startdate (UNIX timestamp)
+    1631184000, -- Replace with the desired value for enddate (UNIX timestamp)
+    'Active', -- Replace with the desired value for status
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
+
+INSERT INTO eg_wms_attendance_register (
+    id,
+    tenantid,
+    registernumber,
+    name,
+    startdate,
+    enddate,
+    status,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '2', -- Replace with the desired value for id
+    'tenant123', -- Replace with the desired value for tenantid
+    '2', -- Replace with the desired value for registernumber
+    'John Doe', -- Replace with the desired value for name
+    1631097600, -- Replace with the desired value for startdate (UNIX timestamp)
+    1631184000, -- Replace with the desired value for enddate (UNIX timestamp)
+    'Active', -- Replace with the desired value for status
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
+
+INSERT INTO eg_wms_attendance_register (
+    id,
+    tenantid,
+    registernumber,
+    name,
+    startdate,
+    enddate,
+    status,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '3', -- Replace with the desired value for id
+    'tenant123', -- Replace with the desired value for tenantid
+    '3', -- Replace with the desired value for registernumber
+    'John Doe', -- Replace with the desired value for name
+    1631097600, -- Replace with the desired value for startdate (UNIX timestamp)
+    1631184000, -- Replace with the desired value for enddate (UNIX timestamp)
+    'Active', -- Replace with the desired value for status
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
+
+INSERT INTO eg_wms_attendance_staff (
+    id,
+    individual_id,
+    register_id,
+    enrollment_date,
+    deenrollment_date,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '1', -- Replace with the desired value for id
+    'staff123', -- Replace with the desired value for individual_id
+    '1', -- Replace with the desired value for register_id
+    1631097600, -- Replace with the desired value for enrollment_date (UNIX timestamp)
+    1631184000, -- Replace with the desired value for deenrollment_date (UNIX timestamp)
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
+
+INSERT INTO eg_wms_attendance_staff (
+    id,
+    individual_id,
+    register_id,
+    enrollment_date,
+    deenrollment_date,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '2', -- Replace with the desired value for id
+    'staff456', -- Replace with the desired value for individual_id
+    '1', -- Replace with the desired value for register_id
+    1631097600, -- Replace with the desired value for enrollment_date (UNIX timestamp)
+    1631184000, -- Replace with the desired value for deenrollment_date (UNIX timestamp)
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
+
+INSERT INTO eg_wms_attendance_attendee (
+    id,
+    individual_id,
+    register_id,
+    enrollment_date,
+    deenrollment_date,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '1', -- Replace with the desired value for id
+    'individual123', -- Replace with the desired value for individual_id
+    '1', -- Replace with the desired value for register_id
+    1631097600, -- Replace with the desired value for enrollment_date (UNIX timestamp)
+    1631184000, -- Replace with the desired value for deenrollment_date (UNIX timestamp)
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
+
+INSERT INTO eg_wms_attendance_attendee (
+    id,
+    individual_id,
+    register_id,
+    enrollment_date,
+    deenrollment_date,
+    additionaldetails,
+    createdby,
+    lastmodifiedby,
+    createdtime,
+    lastmodifiedtime
+) VALUES (
+    '2', -- Replace with the desired value for id
+    'individual123', -- Replace with the desired value for individual_id
+    '2', -- Replace with the desired value for register_id
+    1631097600, -- Replace with the desired value for enrollment_date (UNIX timestamp)
+    1631184000, -- Replace with the desired value for deenrollment_date (UNIX timestamp)
+    'Additional details here', -- Replace with the desired value for additionaldetails
+    'user123', -- Replace with the desired value for createdby
+    'user456', -- Replace with the desired value for lastmodifiedby
+    1631097600, -- Replace with the desired value for createdtime (UNIX timestamp)
+    1631097600 -- Replace with the desired value for lastmodifiedtime (UNIX timestamp)
+);
